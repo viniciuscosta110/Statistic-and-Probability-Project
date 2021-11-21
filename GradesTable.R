d56aadb5569f5dd2630313d82b1172d6e4764fc2
Vinicius Costa Marinho <viniciuscosta1900@hotmail.com>
2021-11-20 21:31:11 -0300

    Add files via upload

--- GradesTable.R
+++ GradesTable.R
@@ -8,7 +8,21 @@ frequel <- prop.table(freq)
 freqAc <- cumsum(freq)
 frequelAc <- cumsum(frequel)
 
+#Tabelas com Notas_ENEM
 tabelaf <- cbind(freq, freqAc, frequel = round(frequel*100, digits=4), frequelAc = round(frequelAc*100, digits=4))
-tabelaf
 
+#Boxplot Notas_ENEM
 boxplot(notas_enem, main = "Notas do Enem", col="blue", border="black")
+
+#Histograma Notas_ENEM
+hist(freq, main = "Frequência de notas", 
+     xlab = "Frequência", ylab = "Quantidade",
+     col="blue", border="black",
+     xlim = c(0, 30), ylim = c(0, 8000), cex.axis=0.8)
+
+#Barplot com quartis e sexo
+notas.cut <- cut(enem_data$NOTA_ENEN, breaks = quantile(enem_data$NOTA_ENEN))
+sexo_notas <- table(enem_data$TP_SEXO, notas.cut)
+barplot(sexo_notas, main = "Nota dividido por sexo", 
+        col=c("purple", "blue"), beside=TRUE, legend=TRUE, 
+        args.legend = list(x = "topright", inset = c(0, -0.4)))
