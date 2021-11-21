84a3f7bb545d8aea448d3e18e9cdbf821ef48af6
tuliocl <58963175+tuliocl@users.noreply.github.com>
2021-11-21 15:58:10 -0300

    Estamos conseguindo

--- Atividade1.R
+++ Atividade1.R
@@ -1,5 +1,6 @@
 install.packages("readxl")
 library("readxl")
+options(scipen=100)
 enem_data <- read_excel("ENEM_AL_EXCEL_AJUS_OK.xlsx")
 notas_enem <- enem_data["NOTA_ENEN"]
 
@@ -20,16 +21,39 @@ hist(freq, main = "Frequência de notas",
      col="blue", border="black",
      xlim = c(0, 30), ylim = c(0, 8000), cex.axis=0.8)
 
-#Barplot com quartis e sexo
+#Barplot com quartis (nota e sexo)
 notas.cut <- cut(enem_data$NOTA_ENEN, breaks = quantile(enem_data$NOTA_ENEN))
 sexo_notas <- table(enem_data$TP_SEXO, notas.cut)
 barplot(sexo_notas, main = "Nota dividido por sexo", 
         col=c("purple", "blue"), beside=TRUE, legend=TRUE, 
         args.legend = list(x = "topright", inset = c(0, -0.4)),ylim = c(0, 12000))
 
+#barplot - idade e nota
+notas.cut <- cut(enem_data$NOTA_ENEN, breaks = quantile(enem_data$NOTA_ENEN))
+idade.cut <- cut(enem_data$NU_IDADE, breaks = quantile(enem_data$NU_IDADE))
+nota_idade <- table(notas.cut,idade.cut)
+barplot(nota_idade, main = "Nota por idade", 
+        col=c("green", "blue","red","purple"), beside=TRUE, legend=TRUE, 
+        args.legend = list(x = "topright", inset = c(0, -0.4)),ylim = c(0, 10000))
 
 #Barplot - Relação sexo e treineiro
 traineiro_idade <- table(enem_data$IN_TREINEIRO,enem_data$TP_SEXO)
 barplot(traineiro_idade, main = "Relação Treineiro X Sexo", 
         col=c("purple", "blue"), beside=TRUE, legend=TRUE, 
         args.legend = list(x = "topright", inset = c(0, -0.4)))
+
+#barplot - Sexo e redação
+##resolver o 1000
+notas_redacao.cut <- cut(enem_data$NU_NOTA_REDACAO, breaks = quantile(enem_data$NU_NOTA_REDACAO))
+sexo_notas <- table(enem_data$TP_SEXO,notas_redacao.cut)
+barplot(sexo_notas, main = "Nota Redação dividida por sexo", 
+        col=c("purple", "blue"), beside=TRUE, legend=TRUE, 
+        args.legend = list(x = "topright", inset = c(0, -0.4)),ylim = c(0, 12000),cex.axis = 0.8)
+
+#algoplor - idade - nota de matematica
+notas_matematica <- cut(enem_data$NU_NOTA_MT, breaks = quantile(enem_data$NU_NOTA_MT))
+idade.cut <- cut(enem_data$NU_IDADE, breaks = quantile(enem_data$NU_IDADE))
+matematica_idade <- table(idade.cut,notas_matematica)
+barplot(matematica_idade, main = "Nota de matematica por idade", 
+        col=c("green", "blue","red","purple"), beside=TRUE, legend=TRUE, 
+        args.legend = list(x = "topright", inset = c(0, -0.4)),ylim = c(0, 10000))
